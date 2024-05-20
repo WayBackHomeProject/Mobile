36e2607ae256b480ef490cdadb1abd9f39c27bf6
PC020\SSAFY <skele1207@gmail.com>
2024-05-20 13:20:47 +0900

    refactor : MainFragment로 Map UI 이동

--- mobile_app/app/src/main/java/com/ssafy/waybackhome/main/MainViewModel.kt
+++ mobile_app/app/src/main/java/com/ssafy/waybackhome/main/MainViewModel.kt
@@ -1,8 +1,15 @@
 package com.ssafy.waybackhome.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.naver.maps.geometry.LatLng
 import com.ssafy.waybackhome.data.LocalRepository
+import com.ssafy.waybackhome.data.cctv.CctvData
+import com.ssafy.waybackhome.data.cctv.CctvService
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
@@ -14,4 +21,12 @@ class MainViewModel : ViewModel() {
     val destinations = repo.selectAllDestination()
 
     var bottomSheetState : Int = BottomSheetBehavior.STATE_COLLAPSED
+
+    private var _cctvs = MutableLiveData<List<CctvData>>()
+    val cctvs : LiveData<List<CctvData>> get() = _cctvs
+    fun getCctvDatas(location : LatLng, radius : Double){
+        viewModelScope.launch {
+            CctvService.api.getCctv(location.latitude, location.longitude, radius)
+        }
+    }
 }
