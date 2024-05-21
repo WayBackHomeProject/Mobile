4de072647142fbe8f88999bcd6fc3bb47f6e0d0d
limnyn <hogandi001@gmail.com>
2024-05-22 00:31:29 +0900

    feat : [UI 수정] MainFragment.kt - cctv, police, destination, alert, convenience icon 추가, MainViewModel.kt - circles 외곽선 추가

--- mobile_app/app/src/main/java/com/ssafy/waybackhome/main/MainFragment.kt
+++ mobile_app/app/src/main/java/com/ssafy/waybackhome/main/MainFragment.kt
@@ -29,6 +29,7 @@ import com.naver.maps.map.NaverMap
 import com.naver.maps.map.NaverMapOptions
 import com.naver.maps.map.NaverMapSdk
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.CircleOverlay
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.FusedLocationSource
